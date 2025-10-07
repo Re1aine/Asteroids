2607195faf2f39b8191e7e5369f37c34cb4d823c
ReLaine <pupkin_vasya1998@mail.ru>
2025-10-07 04:56:36 +0300

    fix StateMachine

--- src/Asteroids/Assets/Code/GameFlow/StateMachine.cs
+++ src/Asteroids/Assets/Code/GameFlow/StateMachine.cs
@@ -8,22 +8,22 @@
         _stateFactory = stateFactory;
     }
     
-    public void Enter<TState>() where TState : class, IState
+    public void Enter<TState>() where TState : IState
     {
         IState state = ChangeState<TState>();
         state?.Enter();
     }
 
-    public void Enter<TState, TArg>(TArg arg) where TState : class, IStateWithArg<TArg>
+    public void Enter<TState, TArg>(TArg arg) where TState : IStateWithArg<TArg>
     {
         TState state = ChangeState<TState>();
         state.Enter(arg);
     }
 
-    private TState GetState<TState>() where TState : class, IExitableState => 
+    private TState GetState<TState>() where TState : IExitableState => 
         _stateFactory.Create<TState>();
 
-    private TState ChangeState<TState>() where TState : class, IExitableState
+    private TState ChangeState<TState>() where TState : IExitableState
     {
         TState state = GetState<TState>();
         
