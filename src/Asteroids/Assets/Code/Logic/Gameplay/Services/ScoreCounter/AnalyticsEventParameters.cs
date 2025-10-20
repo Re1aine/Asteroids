ddaa04ab38fccdf701f75ba653442f01fca77ed0
ReLaine <pupkin_vasya1998@mail.ru>
2025-10-20 20:02:49 +0300

    add AnalyticEventNames, AnalyticsEventParameters

---
+++ src/Asteroids/Assets/Code/Logic/Gameplay/Services/ScoreCounter/AnalyticsEventParameters.cs
@@ -0,0 +1,7 @@
+﻿public static class AnalyticsEventParameters
+{
+    public const string BulletReleaseCount = "BulletReleaseCount";
+    public const string LaserReleaseCount = "LaserReleaseCount";
+    public const string AsteroidKills = "AsteroidKills";
+    public const string UfoKills = "UfoKills";
+}
---
+++ src/Asteroids/Assets/Code/Logic/Gameplay/Services/ScoreCounter/AnalyticsEventsName.cs
@@ -0,0 +1,6 @@
+﻿public static class AnalyticsEventsName
+{
+    public const string StartSession = "StartSession";
+    public const string LaserUsed = "LaserUsed";
+    public const string EndSession = "EndSession";
+}
