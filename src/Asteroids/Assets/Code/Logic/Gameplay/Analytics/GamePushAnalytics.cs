19ab64302d0e1331c3eafd148bac379b28f002a0
ReLaine <pupkin_vasya1998@mail.ru>
2025-11-29 16:50:17 +0300

    add GamePushAnalytics

---
+++ src/Asteroids/Assets/Code/Logic/Gameplay/Analytics/GamePushAnalytics.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using Code.Logic.Gameplay.Analytics;
+using Code.Logic.Gameplay.Analytics.AnalyticsKeys;
+using Code.Logic.Gameplay.Analytics.AnalyticsStore;
+using GamePush;
+using UnityEngine;
+
+public class GamePushAnalytics : IAnalytics
+{
+    private bool _isInitialized;
+
+    public async Task InitializeAsync()
+    {
+        if (_isInitialized)
+            return;
+            
+        try
+        {
+            await GP_Init.Ready;
+            _isInitialized = true;
+            Debug.Log("<b><color=green> GamePush initialized successfully! </color></b>");
+        }
+        catch (Exception e)
+        {
+            _isInitialized = false;
+            Debug.LogException(e);
+        }
+    }
+
+    public void StartSession()
+    {
+        GP_Analytics.Goal(AnalyticsEventsName.StartSession, 0);
+    }
+
+    public void SendLaserUsedEvent()
+    {
+        GP_Analytics.Goal(AnalyticsEventsName.LaserUsed, AnalyticsEventParameters.LaserReleaseCount);
+    }
+
+    public void EndSession(IAnalyticsStore analyticsStore)
+    {
+        GP_Analytics.Goal(AnalyticsEventsName.EndSession, 0);
+    }
+}
--- src/Asteroids/Assets/Code/Scopes/GameplayScope.cs
+++ src/Asteroids/Assets/Code/Scopes/GameplayScope.cs
@@ -34,34 +34,34 @@ namespace Code.Scopes
     public class GameplayScope : LifetimeScope
     {
         [SerializeField] private Camera _camera;
-    
+
         protected override void Configure(IContainerBuilder builder)
         {
             builder.Register<AssetsLoader>(Lifetime.Singleton).As<IAssetsLoader>();
             builder.Register<AssetsProvider>(Lifetime.Singleton).As<IAssetsProvider>();
 
             builder.Register<AnalyticsStore>(Lifetime.Singleton).As<IAnalyticsStore>();
-            builder.Register<FireBaseAnalytics>(Lifetime.Singleton).As<IAnalytics>();
+            InitializeAnalytics(builder);
 
             builder.Register<SaveLoadService>(Lifetime.Singleton).As<ISaveLoadService>();
 
             builder.Register<PlayerRepository>(Lifetime.Singleton).As<IRepository>();
-            
+
             builder.Register<RepositoriesHolder>(Lifetime.Singleton).As<IRepositoriesHolder>();
-            
+
             builder.RegisterComponentInHierarchy<CoroutineRunner>().As<ICoroutineRunner>();
-        
+
             builder.Register<InputService>(Lifetime.Singleton).As<IInputService>();
 
             builder.Register<CameraProvider>(Lifetime.Singleton).As<ICameraProvider>().WithParameter(_camera);
             builder.Register<PlayerProvider>(Lifetime.Singleton).As<IPlayerProvider>();
             builder.Register<HUDProvider>(Lifetime.Singleton).As<IHUDProvider>();
-        
+
             builder.Register<ScreenBoundaries>(Lifetime.Singleton).As<IBoundaries>();
             builder.Register<PointWrapService>(Lifetime.Singleton).As<IPointWrapService>();
 
             builder.Register<ScoreCountService>(Lifetime.Singleton).As<IScoreCountService>();
-        
+
             builder.Register<GameFactory>(Lifetime.Singleton).As<IGameFactory>();
 
             builder.Register<PlayerDeathObserver>(Lifetime.Singleton).As<IPlayerDeathObserver>();
@@ -70,16 +70,25 @@ namespace Code.Scopes
             builder.Register<UFOsHolder>(Lifetime.Singleton).As<IUFOsHolder>();
             builder.Register<AsteroidsHolder>(Lifetime.Singleton).As<IAsteroidsHolder>();
             builder.Register<BulletsHolder>(Lifetime.Singleton).As<IBulletsHolder>();
-        
+
             builder.Register<UFOSpawner>(Lifetime.Singleton).As<IUFOSpawner>();
             builder.Register<AsteroidSpawner>(Lifetime.Singleton).As<IAsteroidSpawner>();
-        
+
             builder.RegisterComponentInHierarchy<BackgroundResizer>();
-        
+
             builder.Register<StateFactory>(Lifetime.Singleton);
             builder.Register<GameplayStateMachine>(Lifetime.Singleton);
-        
+
             builder.RegisterEntryPoint<GameplayEntryPoint>();
+        }
+
+        private void InitializeAnalytics(IContainerBuilder builder)
+        {
+            if (Application.platform == RuntimePlatform.WebGLPlayer)
+                builder.Register<GamePushAnalytics>(Lifetime.Singleton).As<IAnalytics>();
+            else
+                builder.Register<FireBaseAnalytics>(Lifetime.Singleton).As<IAnalytics>();
         }
     }
 }
+
