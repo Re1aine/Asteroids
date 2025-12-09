0b1f26f231b271ced9bfc84095a75bf0b16d919c
ReLaine <pupkin_vasya1998@mail.ru>
2025-12-10 02:43:15 +0300

    add SoundProvider

---
+++ src/Asteroids/Assets/Code/Scopes/SoundProvider.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class SoundProvider : ISoundProvider
+{
+    private readonly AudioConfig _config;
+
+    public SoundProvider(AudioConfig config)
+    {
+        _config = config;
+    }
+
+    public AudioClip GetClipByType(SFXType type)
+    {
+        foreach (var vo in _config.SFXs)
+        {
+            if (vo.type != type)
+                continue;
+
+            var clipsLength = vo.clips.Length;
+            var randomIndex = Random.Range(0, clipsLength);
+            return vo.clips[randomIndex];
+        }
+        
+        throw new Exception($"Doesn't exist settings with type : {type}");
+    }
+}
+
+public interface ISoundProvider
+{
+    AudioClip GetClipByType(SFXType type);
+}
