73bc61d43712937583d14fd7e62acb241921c1d4
ReLaine <pupkin_vasya1998@mail.ru>
2025-12-10 02:41:54 +0300

    add AudioConfig

---
+++ src/Asteroids/Assets/Code/Scopes/AudioConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AudioSFX", menuName = "AudioSFX")]
+public class AudioConfig : ScriptableObject
+{
+    public SoundSettingsVo[] SFXs;
+}
+
+public enum SFXType
+{
+    BulletShoot = 0,
+    LaserShoot = 1,
+    Music = 2
+}
+
+
+[Serializable]
+public struct SoundSettingsVo
+{
+    public SFXType type;
+    public AudioClip[] clips; 
+}
