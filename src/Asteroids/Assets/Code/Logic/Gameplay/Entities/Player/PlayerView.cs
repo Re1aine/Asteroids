c4d80a8185f2f1beaeaa9f654f1e71ea4df6f6ab
ReLaine <pupkin_vasya1998@mail.ru>
2025-12-12 21:28:50 +0300

    fix player movement

--- src/Asteroids/Assets/Code/Logic/Gameplay/Entities/Player/PlayerView.cs
+++ src/Asteroids/Assets/Code/Logic/Gameplay/Entities/Player/PlayerView.cs
@@ -6,7 +6,6 @@ using VContainer;
 
 namespace Code.Logic.Gameplay.Entities.Player
 {
-    [RequireComponent(typeof(Rigidbody2D))]
     public class PlayerView : MonoBehaviour
     {
         public event Action<DamageType>  OnDamageReceived;
@@ -19,9 +18,7 @@ namespace Code.Logic.Gameplay.Entities.Player
         [SerializeField] private float _rotateSpeed;
 
         private IInputService _inputService;
-
-        private Rigidbody2D _rigidbody;
-
+        
         public ReadOnlyReactiveProperty<Vector3> Position => _position;
         public ReadOnlyReactiveProperty<Quaternion> Rotation => _rotation;
         public ReadOnlyReactiveProperty<float> Velocity => _velocity;
@@ -46,8 +43,6 @@ namespace Code.Logic.Gameplay.Entities.Player
         
         private void Awake()
         {
-            _rigidbody = GetComponent<Rigidbody2D>();
-
             _position = new ReactiveProperty<Vector3>();
             _rotation = new ReactiveProperty<Quaternion>();
             _velocity =  new ReactiveProperty<float>();
@@ -71,7 +66,6 @@ namespace Code.Logic.Gameplay.Entities.Player
         private void Start()
         {
             _currentVelocity = Vector2.zero;
-            _rigidbody.linearVelocity = Vector2.zero;
             _moveDirection = Vector2.zero;
         }
 
@@ -80,14 +74,12 @@ namespace Code.Logic.Gameplay.Entities.Player
             _moveDirection = _inputService.Movement;
         
             HandleRotate();
-        }
-    
-        private void FixedUpdate() =>
             HandleMove();
+        }
 
         private void HandleRotate()
         {
-            _rotateAngle -= _inputService.Movement.x * _rotateSpeed;
+            _rotateAngle -= (_inputService.Movement.x * _rotateSpeed) * Time.deltaTime;
             transform.rotation = Quaternion.Euler(0, 0, _rotateAngle);
         }
     
@@ -98,17 +90,17 @@ namespace Code.Logic.Gameplay.Entities.Player
             else 
                 DecelerateMove();
         
-            _rigidbody.linearVelocity = _currentVelocity;
+            transform.position += new Vector3(_currentVelocity.x, _currentVelocity.y, 0) * Time.deltaTime;
         }
     
         private void AccelerateMove()
         {
             Vector2 targetVelocity = (Vector2)transform.up * (_moveDirection.y * _moveSpeed);
-            _currentVelocity = Vector2.Lerp(_currentVelocity, targetVelocity, _accelerationMove * Time.fixedDeltaTime);
+            _currentVelocity = Vector2.Lerp(_currentVelocity, targetVelocity, _accelerationMove * Time.deltaTime); ;
         }
     
         private void DecelerateMove() => 
-            _currentVelocity = Vector2.Lerp(_currentVelocity, Vector2.zero, _decelerationMove  * Time.fixedDeltaTime);
+            _currentVelocity = Vector2.Lerp(_currentVelocity, Vector2.zero, _decelerationMove  * Time.deltaTime);
     
         private void OnCollisionEnter2D(Collision2D other)
         {
