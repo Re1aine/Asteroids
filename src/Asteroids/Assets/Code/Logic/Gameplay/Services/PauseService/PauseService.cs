e4241fc9339cd20678d3866912da95d933918464
ReLaine <pupkin_vasya1998@mail.ru>
2025-12-20 17:20:56 +0300

    add IPauseService

---
+++ src/Asteroids/Assets/Code/Logic/Gameplay/Services/PauseService/PauseService.cs
@@ -0,0 +1,13 @@
+﻿
+public class PauseService : IPauseService
+{
+    public bool IsPaused => _isPaused;
+
+    private bool _isPaused;
+
+    public void Pause() => 
+        _isPaused = true;
+
+    public void UnPause() => 
+        _isPaused = false;
+}
