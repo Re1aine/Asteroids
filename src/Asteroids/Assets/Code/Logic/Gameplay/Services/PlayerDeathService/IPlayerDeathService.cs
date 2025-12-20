3453d50d2d45e984d1b37b09ee7f8b638b9ee5d3
ReLaine <pupkin_vasya1998@mail.ru>
2025-12-20 17:18:01 +0300

    add IPlayerDeathService

---
+++ src/Asteroids/Assets/Code/Logic/Gameplay/Services/PlayerDeathService/IPlayerDeathService.cs
@@ -0,0 +1,4 @@
+﻿public interface IPlayerDeathService
+{
+    void Initialize();
+}
---
+++ src/Asteroids/Assets/Code/Logic/Gameplay/Services/PlayerDeathService/PlayerDeathSerivce.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Code.Logic.Gameplay.Entities;
+using Code.Logic.Gameplay.Entities.Player;
+using Code.Logic.Gameplay.Services.Providers.PlayerProvider;using VContainer.Unity;
+
+public class PlayerDeathService : IPlayerDeathService, IInitializable, IDisposable
+{
+    private readonly IAdsService _adsService;
+    private readonly IPlayerProvider _playerProvider;
+    private readonly IPlayerDeathProcessor _playerDeathProcessor;
+
+    private PlayerPresenter _presenter;
+    
+    public PlayerDeathService(IAdsService adsService, IPlayerProvider playerProvider, IPlayerDeathProcessor playerDeathProcessor)
+    {
+        _adsService = adsService;
+        _playerProvider = playerProvider;
+        _playerDeathProcessor = playerDeathProcessor;
+        
+        _playerProvider.PlayerChanged += OnPlayerChanged;
+        _adsService.AdCompleted += OnAdCompleted;
+    }
+
+    public void Initialize() => 
+        _presenter.DamageReceiver.LethalDamageReceived += OnReceivedLethalDamage;
+
+    private void OnPlayerChanged(PlayerPresenter presenter) => 
+        _presenter = presenter;
+
+    private void OnAdCompleted(AdContext context)
+    {
+        if (context == AdContext.DeathRevive)
+            _playerDeathProcessor.CancelProcess();
+        else
+            _playerDeathProcessor.CompleteProcess();
+    }
+
+    private void OnReceivedLethalDamage(DamageType damageType) => 
+        _playerDeathProcessor.StartProcess(damageType);
+
+    public void Dispose()
+    {
+        _playerProvider.PlayerChanged -= OnPlayerChanged;
+        _adsService.AdCompleted -= OnAdCompleted;
+        _presenter.DamageReceiver.LethalDamageReceived -= OnReceivedLethalDamage;
+    }
+}
