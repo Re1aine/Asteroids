9bee7a28ea1f3fa3198f9fd12dc9ffe6430706b0
ReLaine <pupkin_vasya1998@mail.ru>
2025-12-23 21:08:21 +0300

    add ISDKInitializer

---
+++ src/Asteroids/Assets/Code/Logic/Gameplay/Services/AdService/ISDKInitializer.cs
@@ -0,0 +1,8 @@
+﻿using Cysharp.Threading.Tasks;
+
+public interface ISDKInitializer
+{
+    UniTask Initialize();
+    bool IsGamePushInitialized { get; }
+    bool IsFireBaseInitialized { get; }
+}
---
+++ src/Asteroids/Assets/Code/Logic/Gameplay/Services/AdService/SDKInitializer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using Firebase;
+using GamePush;
+using UnityEngine;
+
+public class SDKInitializer : ISDKInitializer
+{
+    private FirebaseApp _firebaseApp;
+
+    public bool IsGamePushInitialized {get; private set;}
+    public bool IsFireBaseInitialized {get; private set;}
+    
+    public async UniTask Initialize()
+    {
+        await InitializeGamePushSDK();
+        await InitializerFireBaseSDK();
+    }
+
+    private async UniTask InitializeGamePushSDK()
+    {
+        try
+        {
+            await GP_Init.Ready;
+            Debug.Log("<b><color=green> [GamePush initialized successfully] </color></b>");
+            IsGamePushInitialized = true;
+        }
+        catch (Exception e)
+        {
+            Debug.LogException(e);
+            IsGamePushInitialized = false;
+        }          
+    }
+    
+
+    private async UniTask InitializerFireBaseSDK()
+    {
+        try
+        {
+            var dependencyStatus = await FirebaseApp.CheckAndFixDependenciesAsync();
+            if (dependencyStatus != DependencyStatus.Available)
+            {
+                Debug.LogError($"Failed to initialize Firebase: {dependencyStatus}");
+                IsFireBaseInitialized = false;;
+            }
+       
+            _firebaseApp = FirebaseApp.DefaultInstance;
+            Debug.Log("<b><color=green> Firebase initialized successfully! </color></b>");
+            IsFireBaseInitialized = true;;
+        }
+        catch (Exception e)
+        {
+            Debug.LogException(e);
+            IsFireBaseInitialized = false;
+        }
+    } 
+}
