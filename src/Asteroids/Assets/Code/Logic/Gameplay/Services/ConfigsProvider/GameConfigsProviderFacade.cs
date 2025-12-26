52bb7019b21cdedfb70797a06864a012389ddbff
ReLaine <pupkin_vasya1998@mail.ru>
2025-12-27 00:40:31 +0300

    add IGameConfigsProvider

---
+++ src/Asteroids/Assets/Code/Logic/Gameplay/Services/ConfigsProvider/GameConfigsProviderFacade.cs
@@ -0,0 +1,30 @@
+﻿using Code.Logic.Gameplay.Audio;
+using Code.Logic.Gameplay.Services.ConfigsProvider;
+using Cysharp.Threading.Tasks;
+
+public class GameConfigsProviderFacade : IGameConfigsProvider
+{
+    private readonly IGameAssetsConfigsProvider _assetsConfigsProvider;
+    private readonly IGameBalanceConfigsProvider _balanceConfigsProvider;
+    
+    public AudioConfig AudioConfig => _assetsConfigsProvider.AudioConfig;
+    public VFXConfig VFXConfig => _assetsConfigsProvider.VFXConfig;
+    public PlayerConfig PlayerConfig => _balanceConfigsProvider.PlayerConfig;
+    public AsteroidConfig AsteroidConfig => _balanceConfigsProvider.AsteroidConfig;
+    public GunConfig GunConfig => _balanceConfigsProvider.GunConfig;
+    public UfoConfig UfoConfig => _balanceConfigsProvider.UfoConfig;
+    public UfoSpawnerConfig UfoSpawnerConfig => _balanceConfigsProvider.UfoSpawnerConfig;
+    public AsteroidSpawnerConfig AsteroidSpawnerConfig => _balanceConfigsProvider.AsteroidSpawnerConfig;
+    
+    public GameConfigsProviderFacade(IGameAssetsConfigsProvider assetsConfigsProvider, IGameBalanceConfigsProvider balanceConfigsProvider)
+    {
+        _assetsConfigsProvider = assetsConfigsProvider;
+        _balanceConfigsProvider = balanceConfigsProvider;
+    }
+
+    public async UniTask Initialize()
+    {
+        await _assetsConfigsProvider.Initialize();
+        await _balanceConfigsProvider.Initialize();
+    }
+}
---
+++ src/Asteroids/Assets/Code/Logic/Gameplay/Services/ConfigsProvider/IGameConfigsProvider.cs
@@ -0,0 +1,16 @@
+﻿using Code.Logic.Gameplay.Audio;
+using Code.Logic.Gameplay.Services.ConfigsProvider;
+using Cysharp.Threading.Tasks;
+
+public interface IGameConfigsProvider
+{
+    UniTask Initialize();
+    AudioConfig AudioConfig { get; }
+    VFXConfig VFXConfig { get; } 
+    PlayerConfig PlayerConfig { get; }
+    AsteroidConfig AsteroidConfig { get; }
+    GunConfig GunConfig { get; }
+    UfoConfig UfoConfig { get; }
+    UfoSpawnerConfig UfoSpawnerConfig { get; }
+    AsteroidSpawnerConfig AsteroidSpawnerConfig { get; }
+}
