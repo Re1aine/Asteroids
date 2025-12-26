09f44ca1bdc977471814b77c7c7806ed1a9f07e6
ReLaine <pupkin_vasya1998@mail.ru>
2025-12-27 00:35:25 +0300

    add IGameBalanceConfigsProvider

---
+++ src/Asteroids/Assets/Code/Logic/Gameplay/Services/ConfigsProvider/GameBalanceConfigsProvider/IGameBalanceConfigsProvider.cs
@@ -0,0 +1,12 @@
+﻿using Cysharp.Threading.Tasks;
+
+public interface IGameBalanceConfigsProvider  
+{
+    UniTask Initialize();
+    PlayerConfig PlayerConfig { get; }
+    AsteroidConfig AsteroidConfig { get; }
+    GunConfig GunConfig { get; }
+    UfoConfig UfoConfig { get; }
+    UfoSpawnerConfig UfoSpawnerConfig { get; }
+    AsteroidSpawnerConfig AsteroidSpawnerConfig { get; }
+}
