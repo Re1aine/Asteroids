7e3171720e215afe16298fd396af6b5996e0d997
ReLaine <pupkin_vasya1998@mail.ru>
2026-01-05 21:21:40 +0300

    add SceneLoader

---
+++ src/Asteroids/Assets/Code/EntryPoints/ISceneLoader.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+
+public interface ISceneLoader
+{
+    UniTask LoadScene(GameScenes scene);
+    void LoadAnyScene(GameScenes scene, Action onLoaded = null);
+}
---
+++ src/Asteroids/Assets/Code/EntryPoints/SceneLoader.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using Code.Infrastructure.Common.CoroutineService;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.ResourceManagement.ResourceProviders;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : ISceneLoader
+{
+    private readonly ICoroutineRunner _coroutineRunner;
+
+    public SceneLoader(ICoroutineRunner coroutineRunner) => 
+        _coroutineRunner = coroutineRunner;
+
+    public async UniTask LoadScene(GameScenes scene)
+    {
+        AsyncOperationHandle<SceneInstance> handle =
+            Addressables.LoadSceneAsync(scene.ToString(), LoadSceneMode.Single, false);
+
+        await handle.ToUniTask();
+        Debug.Log($"Scene {scene} loaded.");
+        
+        await handle.Result.ActivateAsync().ToUniTask();
+        Debug.Log($"Scene {scene} activated.");
+    }
+
+    public void LoadAnyScene(GameScenes scene, Action onLoaded) => 
+        _coroutineRunner.StartCoroutine(LoadSAnySceneAsync(scene, onLoaded), CoroutineScopes.Global);
+
+    private IEnumerator LoadSAnySceneAsync(GameScenes scene, Action onLoaded)
+    {
+        AsyncOperation asyncSceneLoading  = SceneManager.LoadSceneAsync(scene.ToString());
+        asyncSceneLoading!.allowSceneActivation= false;
+        
+        yield return new WaitUntil(() => asyncSceneLoading!.isDone);
+        Debug.Log($"Scene {scene} loaded.");
+        
+        asyncSceneLoading.allowSceneActivation = true;
+        Debug.Log($"Scene {scene} activated.");
+        
+        yield return null;
+        onLoaded?.Invoke();
+    }
+}
+
+public enum GameScenes
+{
+    Project = 0,
+    Menu = 1,
+    Gameplay = 2
+}
