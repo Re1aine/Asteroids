ad580599783bcb840967555caaa7203b5379398c
ReLaine <pupkin_vasya1998@mail.ru>
2026-01-05 20:45:18 +0300

    add scopes ProjectScope/MenuScope

---
+++ src/Asteroids/Assets/Code/Scopes/MenuScope.cs
@@ -0,0 +1,28 @@
+﻿using Code.GameFlow;
+using Code.Infrastructure.Common.AssetsManagement.AssetProvider;
+using Code.Logic.Gameplay.Services.Providers.HUDProvider;
+using VContainer;
+using VContainer.Unity;
+
+public class MenuScope : LifetimeScope
+{
+    protected override void Configure(IContainerBuilder builder)
+    {
+        builder.Register<AddressablesAssetsProvider>(Lifetime.Singleton).As<IAddressablesAssetsProvider>();
+        
+        builder.Register<PurchaseCatalog>(Lifetime.Singleton).As<IPurchaseCatalog>();
+        builder.Register<GamePushPurchaseService>(Lifetime.Singleton).As<IPurchaseService>();
+        builder.Register<PurchaseHandler>(Lifetime.Singleton).AsSelf();
+        
+        builder.Register<MenuUIFactory>(Lifetime.Singleton)
+            .As<IMenuUIFactory>()
+            .As<IUIFactory>();
+        
+        builder.Register<HUDProvider>(Lifetime.Singleton).As<IHUDProvider>();
+        
+        builder.Register<StateFactory>(Lifetime.Singleton);
+        builder.Register<MenuStateMachine>(Lifetime.Singleton);
+        
+        builder.RegisterEntryPoint<MenuEntryPoint>();
+    }
+}
---
+++ src/Asteroids/Assets/Code/Scopes/ProjectScope.cs
@@ -0,0 +1,44 @@
+﻿using Code.GameFlow;
+using Code.GameFlow.States.Core;
+using Code.Infrastructure.Common.AssetsManagement.AssetLoader;
+using Code.Infrastructure.Common.CoroutineService;
+using Code.Logic.Gameplay.Services.Holders.RepositoriesHolder;
+using Code.Logic.Gameplay.Services.Repository;
+using Code.Logic.Gameplay.Services.Repository.Player;
+using Code.Logic.Gameplay.Services.SaveLoad;
+using Code.Logic.Gameplay.Services.SDKInitializer;
+using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+
+public class ProjectScope : LifetimeScope
+{
+    [SerializeField] private CoroutineRunner _coroutineRunner;
+
+    protected override void Awake()
+    {
+        DontDestroyOnLoad(this);
+        base.Awake();
+    }
+
+    protected override void Configure(IContainerBuilder builder)
+    {
+        builder.RegisterComponentInNewPrefab(_coroutineRunner, Lifetime.Singleton).As<ICoroutineRunner>();
+
+        builder.Register<AddressablesAssetsLoader>(Lifetime.Singleton).As<IAddressablesAssetsLoader>();
+
+        builder.Register<SaveLoadService>(Lifetime.Singleton).As<ISaveLoadService>();
+        
+        builder.Register<PlayerRepository>(Lifetime.Singleton).As<IRepository>();
+        builder.Register<RepositoriesHolder>(Lifetime.Singleton).As<IRepositoriesHolder>();
+
+        builder.Register<SDKInitializer>(Lifetime.Singleton).As<ISDKInitializer>();
+        
+        builder.Register<SceneLoader>(Lifetime.Singleton).As<ISceneLoader>();
+
+        builder.Register<StateFactory>(Lifetime.Singleton);
+        builder.Register<GameStateMachine>(Lifetime.Singleton);
+
+        builder.RegisterEntryPoint<ProjectEntryPoint>();
+    }
+}
