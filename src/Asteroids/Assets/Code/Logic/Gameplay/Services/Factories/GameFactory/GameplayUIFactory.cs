853f9cd59c3230ab50f1934c1a20716fe8780768
ReLaine <pupkin_vasya1998@mail.ru>
2026-01-05 21:37:07 +0300

    add GameplayUIFactory and MenuUIFactory

---
+++ src/Asteroids/Assets/Code/Logic/Gameplay/Services/Factories/GameFactory/GameplayUIFactory.cs
@@ -0,0 +1,74 @@
+﻿using Code.Infrastructure.Common.AssetsManagement;
+using Code.Infrastructure.Common.AssetsManagement.AssetProvider;
+using Code.Logic.Gameplay.Services.AdService;
+using Code.Logic.Gameplay.Services.Factories.GameFactory;
+using Code.Logic.Gameplay.Services.Providers.HUDProvider;
+using Code.Logic.Gameplay.Services.Providers.PlayerProvider;
+using Code.Logic.Gameplay.Services.ScoreCounter;
+using Code.UI.HUD;
+using Code.UI.LoseWindow;
+using Code.UI.PlayerStatsWindow;
+using Cysharp.Threading.Tasks;
+using VContainer;
+
+public class GameplayUIFactory : IGameplayUIFactory
+{
+    private readonly IAddressablesAssetsProvider _addressablesAssetsProvider;
+    private readonly IScoreCountService _scoreCountService;
+    private readonly IObjectResolver _resolver;
+
+    public GameplayUIFactory(
+        IAddressablesAssetsProvider addressablesAssetsProvider,
+        IScoreCountService scoreCountService,
+        IObjectResolver resolver)
+    {
+        _addressablesAssetsProvider = addressablesAssetsProvider;
+        _scoreCountService = scoreCountService;
+        _resolver = resolver;
+    }
+    
+    public async UniTask<AHUDPresenter> CreateHUD()
+    {
+        GameplayHUDView view =  await _addressablesAssetsProvider.Instantiate<GameplayHUDView>(
+            AssetsAddress.GameplayHUD);
+            
+        GameplayHUDModel model = new GameplayHUDModel(new GameplayHUDService(this));
+            
+        return new GameplayHUDPresenter(model, view);
+    }
+    
+    public async UniTask<LoseWindowPresenter> CreateLoseWindow()
+    {
+        LoseWindowView view =  await _addressablesAssetsProvider.Instantiate<LoseWindowView>(
+            AssetsAddress.LoseWindow,
+            _resolver.Resolve<IHUDProvider>().HUD.View.transform);
+            
+        LoseWindowModel model = new LoseWindowModel(
+            _scoreCountService);
+            
+        return new LoseWindowPresenter(model, view);
+    }
+
+    public async UniTask<PlayerStatsWindowPresenter> CreatePlayerStatsWindow()
+    {
+        PlayerStatsWindowView view = await _addressablesAssetsProvider.Instantiate<PlayerStatsWindowView>(
+            AssetsAddress.PlayerStatsWindow,
+            _resolver.Resolve<IHUDProvider>().HUD.View.transform);
+            
+        PlayerStatsWindowModel model = new PlayerStatsWindowModel(
+            _resolver.Resolve<IPlayerProvider>());
+            
+        return new PlayerStatsWindowPresenter(model, view);
+    }
+    
+    public async UniTask<ReviveWindowPresenter> CreateReviveWindow()
+    {
+        ReviveWindowView view = await _addressablesAssetsProvider.Instantiate<ReviveWindowView>(
+            AssetsAddress.ReviveWindow, 
+            _resolver.Resolve<IHUDProvider>().HUD.View.transform);
+
+        ReviveWindowModel model = new ReviveWindowModel(_resolver.Resolve<IAdsService>());
+            
+        return new ReviveWindowPresenter(model, view);
+    }
+}
---
+++ src/Asteroids/Assets/Code/Scopes/MenuUIFactory.cs
@@ -0,0 +1,37 @@
+﻿using Code.Infrastructure.Common.AssetsManagement;
+using Code.Infrastructure.Common.AssetsManagement.AssetProvider;
+using Code.Logic.Gameplay.Services.Providers.HUDProvider;
+using Cysharp.Threading.Tasks;
+using VContainer;
+
+public class MenuUIFactory : IMenuUIFactory
+{
+    private readonly IAddressablesAssetsProvider _addressablesAssetsProvider;
+    private readonly IObjectResolver _resolver;
+
+    public MenuUIFactory(IAddressablesAssetsProvider addressablesAssetsProvider, IObjectResolver resolver)
+    {
+        _addressablesAssetsProvider = addressablesAssetsProvider;
+        _resolver = resolver;
+    }
+    
+    public async UniTask<AHUDPresenter> CreateHUD()
+    {
+        MenuHUDView view =  await _addressablesAssetsProvider.Instantiate<MenuHUDView>(AssetsAddress.MenuHUD);
+            
+        MenuHUDModel model = new MenuHUDModel(new MenuHUDService(this));
+            
+        return new MenuHUDPresenter(model, view);
+    }
+
+    public async UniTask<MenuWindowPresenter> CreateMenuWindow()
+    {
+        MenuWindowView view = await _addressablesAssetsProvider.Instantiate<MenuWindowView>(
+            AssetsAddress.MenuWindow,
+            _resolver.Resolve<IHUDProvider>().HUD.View.transform);
+
+        MenuWindowModel model = new MenuWindowModel();
+        
+        return new MenuWindowPresenter(model, view);
+    }
+}
