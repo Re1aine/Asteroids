5767b409fe1780881c97cfaeca351c3016f3af6f
ReLaine <pupkin_vasya1998@mail.ru>
2026-01-05 21:09:58 +0300

    add MenuWindow

---
+++ src/Asteroids/Assets/Code/UI/LoseWindow/MenuWindowPresenter.cs
@@ -0,0 +1,18 @@
+﻿public class MenuWindowPresenter
+{
+    public MenuWindowModel Model { get; }
+    public MenuWindowView View { get; }
+
+    public MenuWindowPresenter(MenuWindowModel model, MenuWindowView view)
+    {
+        Model = model;
+        View = view;
+    }
+
+    public void Destroy()
+    {
+        Model.Dispose();
+        View.Destroy();
+    }
+    
+}
---
+++ src/Asteroids/Assets/Code/UI/LoseWindow/MenuWindowView.cs
@@ -0,0 +1,33 @@
+﻿using Cysharp.Threading.Tasks;
+using R3;
+using UnityEngine;
+using UnityEngine.UI;
+using VContainer;
+
+public class MenuWindowView : MonoBehaviour
+{
+    [SerializeField] private Button _playButton;
+    [SerializeField] private Button _exitButton;
+    
+    private MenuStateMachine _menuStateMachine;
+
+    [Inject]
+    public void Construct(MenuStateMachine menuStateMachine)
+    {
+        _menuStateMachine =  menuStateMachine;
+    }
+    
+    private void Start()
+    {
+        _playButton.OnClickAsObservable()
+            .Subscribe(_ => _menuStateMachine.Enter<LoadSceneState, GameScenes>(GameScenes.Gameplay).Forget())
+            .AddTo(this);;
+
+        _exitButton.OnClickAsObservable()
+            .Subscribe(_ => Debug.Log("Exit"))
+            .AddTo(this);;
+    }
+
+    public void Destroy() => 
+        Destroy(gameObject);
+}
