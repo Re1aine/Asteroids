9308fcb6d022d4a6c18ce6bbd8c379b16c65208f
ReLaine <pupkin_vasya1998@mail.ru>
2026-01-05 21:23:38 +0300

    add SceneHelper

---
+++ src/Asteroids/Assets/Code/Tools/SceneHelper.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public sealed class SceneHelper : MonoBehaviour
+{
+    private const GameScenes InitProjectScene = GameScenes.Project;
+    
+    private void Awake()
+    {
+        if (FindFirstObjectByType<ProjectScope>() != null)
+            return;
+        
+        SceneManager.LoadScene(InitProjectScene.ToString());
+    }
+}
