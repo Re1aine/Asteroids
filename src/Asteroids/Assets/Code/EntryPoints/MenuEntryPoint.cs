19a76967c45a1b6628a598d6ffde95091eb1e01c
ReLaine <pupkin_vasya1998@mail.ru>
2026-01-05 20:51:39 +0300

    add ProjectEntryPoint/MenuEntryPoint

---
+++ src/Asteroids/Assets/Code/EntryPoints/MenuEntryPoint.cs
@@ -0,0 +1,18 @@
+﻿using Cysharp.Threading.Tasks;
+using VContainer.Unity;
+public class MenuEntryPoint : IStartable
+{
+    private readonly MenuStateMachine _menuStateMachine;
+
+    public MenuEntryPoint(MenuStateMachine menuStateMachine)
+    {
+        _menuStateMachine = menuStateMachine;
+    }
+    
+    public void Start()
+    {
+        _menuStateMachine
+            .Enter<MenuInitState>()
+            .Forget();
+    }
+}
---
+++ src/Asteroids/Assets/Code/EntryPoints/ProjectEntryPoint.cs
@@ -0,0 +1,20 @@
+﻿using Code.GameFlow.States.Core;
+using Cysharp.Threading.Tasks;
+using VContainer.Unity;
+
+public class ProjectEntryPoint : IStartable
+{
+    private readonly GameStateMachine _gameStateMachine;
+
+    public ProjectEntryPoint(GameStateMachine gameStateMachine)
+    {
+        _gameStateMachine = gameStateMachine;
+    }
+    
+    public void Start()
+    {
+        _gameStateMachine
+            .Enter<ProjectInitState>()
+            .Forget();
+    }
+}
