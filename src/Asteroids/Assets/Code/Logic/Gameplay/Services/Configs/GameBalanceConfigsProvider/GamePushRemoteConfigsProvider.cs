73d7911974ce43c12c73d7567c6d0767eed5b595
ReLaine <pupkin_vasya1998@mail.ru>
2026-01-08 03:43:10 +0300

    fix GamePushRemoteConfigsProvider

--- src/Asteroids/Assets/Code/Logic/Gameplay/Services/Configs/GameBalanceConfigsProvider/GamePushRemoteConfigsProvider.cs
+++ src/Asteroids/Assets/Code/Logic/Gameplay/Services/Configs/GameBalanceConfigsProvider/GamePushRemoteConfigsProvider.cs
@@ -1,7 +1,6 @@
 ﻿using Code.Logic.Gameplay.Services.Configs.Configs.GameBalance;
 using Code.Logic.Services.SDKInitializer;
 using Cysharp.Threading.Tasks;
-using GamePush;
 using UnityEngine;
 
 namespace Code.Logic.Gameplay.Services.Configs.GameBalanceConfigsProvider
@@ -14,16 +13,16 @@ namespace Code.Logic.Gameplay.Services.Configs.GameBalanceConfigsProvider
         private const string UfoConfigKey = "UfoConfig";
         private const string UfoSpawnerConfigKey = "UfoSpawnerConfig";
         private const string AsteroidSpawnerConfigKey = "AsteroidSpawnerConfig";
-    
+
         public PlayerConfig PlayerConfig { get; private set; }
-        public AsteroidConfig AsteroidConfig {get; private set; }
-        public GunConfig GunConfig {get; private set; }
-        public UfoConfig UfoConfig {get; private set; }
+        public AsteroidConfig AsteroidConfig { get; private set; }
+        public GunConfig GunConfig { get; private set; }
+        public UfoConfig UfoConfig { get; private set; }
         public UfoSpawnerConfig UfoSpawnerConfig { get; private set; }
         public AsteroidSpawnerConfig AsteroidSpawnerConfig { get; private set; }
-    
+
         private readonly ISDKInitializer _sdkInitializer;
-    
+
         private bool _isInitialized;
 
         public GamePushRemoteConfigsProvider(ISDKInitializer sdkInitializer)
@@ -33,7 +32,7 @@ namespace Code.Logic.Gameplay.Services.Configs.GameBalanceConfigsProvider
 
         public UniTask Initialize()
         {
-            if(_isInitialized)
+            if (_isInitialized)
                 return default;
 
             if (_sdkInitializer.IsGamePushInitialized)
@@ -46,14 +45,14 @@ namespace Code.Logic.Gameplay.Services.Configs.GameBalanceConfigsProvider
                 _isInitialized = false;
                 Debug.Log("<b><color=red> [Remote Config is not initialized, using defaults] </color></b>");
             }
-        
+
             LoadConfigs();
             return default;
         }
-    
-        private bool IsCanFetch() => 
+
+        private bool IsCanFetch() =>
             _isInitialized;
-        
+
         private void LoadConfigs()
         {
             PlayerConfig = GetConfig<PlayerConfig>(PlayerConfigKey);
@@ -61,14 +60,18 @@ namespace Code.Logic.Gameplay.Services.Configs.GameBalanceConfigsProvider
             UfoConfig = GetConfig<UfoConfig>(UfoConfigKey);
             GunConfig = GetConfig<GunConfig>(GunConfigKey);
             UfoSpawnerConfig = GetConfig<UfoSpawnerConfig>(UfoSpawnerConfigKey);
-            AsteroidSpawnerConfig =  GetConfig<AsteroidSpawnerConfig>(AsteroidSpawnerConfigKey);        
+            AsteroidSpawnerConfig = GetConfig<AsteroidSpawnerConfig>(AsteroidSpawnerConfigKey);
         }
 
         private T GetConfig<T>(string key) where T : new()
         {
+#if UNITY_EDITOR
+            return new T();
+#else
             return IsCanFetch() && GP_Variables.Has(key) ? 
                 JsonUtility.FromJson<T>(GP_Variables.GetString(key))
                 : new T();
+#endif
         }
     }
 }
