1766e99eea147e94295c1c26cfe7b83d46c1fb97
ReLaine <pupkin_vasya1998@mail.ru>
2026-01-18 08:06:07 +0300

    add AuthHandler

---
+++ src/Asteroids/Assets/Code/Scopes/AuthHandler.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Code.GameFlow.States.Menu;
+using Code.Infrastructure.Common.LogService;
+using Code.Logic.Gameplay.Services.Holders.RepositoriesHolder;
+using Code.Logic.Services.Repository.Player;
+using Cysharp.Threading.Tasks;
+using GamePush;
+using VContainer.Unity;
+
+public class AuthHandler : IInitializable, IDisposable
+{
+    private const string AnonymousAuthKey = "secretCode";
+
+    private readonly IAuthentification _authentification;
+    private readonly SecretCodeDeliverer _secretCodeDeliverer;
+    private readonly MenuStateMachine _menuStateMachine;
+    private readonly ILogService _logService;
+
+    public AuthHandler(IAuthentification authentification,
+        SecretCodeDeliverer secretCodeDeliverer,
+        MenuStateMachine menuStateMachine,
+        ILogService logService)
+    {
+        _authentification = authentification;
+        _secretCodeDeliverer = secretCodeDeliverer;
+        _menuStateMachine = menuStateMachine;
+        _logService = logService;
+    }
+    
+    public void Initialize()
+    {
+        _authentification.AuthStarted += OnAuthStarted;
+        _authentification.AuthCompleted += OnAuthCompleted;
+        
+        GP_Player.OnLoginComplete += OnLoginComplete;
+        GP_Player.OnLoginError += OnLoginError;
+        
+        GP_Player.OnLogoutComplete += OnLogoutComplete;
+        GP_Player.OnLogoutError += OnLogoutError;
+    }
+
+    private void OnAuthStarted()
+    {
+        if (!GP_Platform.IsSecretCodeAuthAvailable())
+            return;
+        
+        _secretCodeDeliverer.SetSecretCode(GP_Player.GetString(AnonymousAuthKey));
+        _secretCodeDeliverer.gameObject.SetActive(true);
+    }
+
+    private void OnAuthCompleted()
+    {
+        _secretCodeDeliverer.gameObject.SetActive(false);
+        _menuStateMachine.Enter<MenuInitState>().Forget();
+    }
+    
+    private void OnLoginComplete()
+    {
+        _logService.LogInfo("Login complete");
+        OnAuthCompleted();
+    }
+    
+    private void OnLoginError() => 
+        _logService.LogError("Login error");
+
+    private void OnLogoutComplete()
+    {
+        
+    }
+
+    private void OnLogoutError()
+    {
+        
+    }
+
+    public void Dispose()
+    {
+        _authentification.AuthStarted -= OnAuthStarted;
+        _authentification.AuthCompleted -= OnAuthCompleted;
+        
+        GP_Player.OnLoginComplete -= OnLoginComplete;
+        GP_Player.OnLoginError -= OnLoginError;
+        
+        GP_Player.OnLogoutComplete -= OnLogoutComplete;
+        GP_Player.OnLogoutError -= OnLogoutError;
+    }
+}
