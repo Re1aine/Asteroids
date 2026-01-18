15e2cabbc73d8f10731224b1fe6722dcc37e8a51
ReLaine <pupkin_vasya1998@mail.ru>
2026-01-18 08:13:29 +0300

    add ICloudSaveLoadStrategy

---
+++ src/Asteroids/Assets/Code/Logic/Services/SaveLoad/ICloudSaveLoadStrategy.cs
@@ -0,0 +1,9 @@
+﻿using Code.Logic.Services.Repository.Player;
+using Cysharp.Threading.Tasks;
+using R3;
+
+public interface ICloudSaveLoadStrategy : ISaveLoadStrategy
+{
+    Observable<PlayerSaveData> SyncFallBack { get; }
+    UniTask<bool> IsAvailable();
+}
