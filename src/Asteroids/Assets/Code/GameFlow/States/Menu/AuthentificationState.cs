8302df821d7a0f6392f5e4b85a2cea4c67f4fc14
ReLaine <pupkin_vasya1998@mail.ru>
2026-01-18 08:06:42 +0300

    add AuthentificationState

---
+++ src/Asteroids/Assets/Code/GameFlow/States/Menu/AuthentificationState.cs
@@ -0,0 +1,31 @@
+﻿using Code.GameFlow.States;
+using Cysharp.Threading.Tasks;
+
+#if !UNITY_EDITOR && UNITY_WEBGL
+using UnityEngine;
+#endif
+
+public class AuthentificationState : IState
+{
+    private readonly IAuthentification _authentification;
+    private readonly AuthHandler _authHandler;
+
+    public AuthentificationState(IAuthentification authentification, AuthHandler authHandler)
+    {
+        _authentification = authentification;
+        _authHandler = authHandler;
+    }
+
+    public UniTask Enter()
+    {
+        _authentification.Initialize();
+        _authHandler.Initialize();
+        
+        _authentification.Login();
+        
+        return default;
+    }
+
+    public UniTask Exit() => 
+        default;
+}
