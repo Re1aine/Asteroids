409fcc77fbbfcfd707d1260e9982af00f4d5850a
ReLaine <pupkin_vasya1998@mail.ru>
2026-01-18 08:05:36 +0300

    add IAuthentification

---
+++ src/Asteroids/Assets/Code/Scopes/IAuthentification.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public interface IAuthentification
+{
+    event Action AuthStarted;
+    event Action AuthCompleted;
+    void Initialize();
+    void Login();
+    void Logout();
+}
