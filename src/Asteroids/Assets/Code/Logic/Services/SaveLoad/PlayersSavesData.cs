735673c88437ccd0e26a30614f208e2c9a1740a9
ReLaine <pupkin_vasya1998@mail.ru>
2026-01-18 08:18:16 +0300

    add PlayersSavesData, PlayersSavesDataWrapper for LocalSaveLoadStrategy

---
+++ src/Asteroids/Assets/Code/Logic/Services/SaveLoad/PlayersSavesData.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+
+[Serializable]
+public class PlayersSavesData
+{
+    public List<PlayersSavesDataWrapper> Datas = new();
+}
---
+++ src/Asteroids/Assets/Code/Logic/Services/SaveLoad/PlayersSavesDataWrapper.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using Code.Logic.Services.Repository.Player;
+
+[Serializable]
+public class PlayersSavesDataWrapper
+{
+    public string Key;
+    public PlayerSaveData Data;
+}
