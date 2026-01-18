fe34323d35b3e19ae122be2bca6d71efb973bd6a
ReLaine <pupkin_vasya1998@mail.ru>
2026-01-18 09:39:30 +0300

    update ProjectScope, MenuScope

--- src/Asteroids/Assets/Code/Scopes/MenuScope.cs
+++ src/Asteroids/Assets/Code/Scopes/MenuScope.cs
@@ -8,6 +8,7 @@ using Code.Logic.Menu.Services.Purchase.Handler;
 using Code.Logic.Services.HUDProvider;
 using Code.UI.UIFactory;
 using Code.UI.UIFactory.MenuUIFactory;
+using UnityEngine;
 using VContainer;
 using VContainer.Unity;
 
@@ -15,10 +16,16 @@ namespace Code.Scopes
 {
     public class MenuScope : LifetimeScope
     {
+        [SerializeField] private SecretCodeDeliverer _codeDeliverer;
+        
         protected override void Configure(IContainerBuilder builder)
         {
             builder.Register<AddressablesAssetsProvider>(Lifetime.Singleton).As<IAddressablesAssetsProvider>();
-        
+            
+            builder.RegisterComponentInNewPrefab(_codeDeliverer, Lifetime.Singleton);
+            
+            builder.Register<AuthHandler>(Lifetime.Singleton).AsSelf();
+            
             builder.Register<PurchaseCatalog>(Lifetime.Singleton).As<IPurchaseCatalog>();
             builder.Register<GamePushPurchaseService>(Lifetime.Singleton).As<IPurchaseService>();
             builder.Register<PurchaseHandler>(Lifetime.Singleton).AsSelf();
@@ -28,7 +35,7 @@ namespace Code.Scopes
                 .As<IUIFactory>();
         
             builder.Register<HUDProvider>(Lifetime.Singleton).As<IHUDProvider>();
-        
+            
             builder.Register<StateFactory>(Lifetime.Singleton);
             builder.Register<MenuStateMachine>(Lifetime.Singleton);
         
