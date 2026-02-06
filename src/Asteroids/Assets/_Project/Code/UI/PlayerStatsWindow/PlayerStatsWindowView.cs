addb7beecd92c798bf972095abb18a260bb3b8f6
ReLaine <pupkin_vasya1998@mail.ru>
2026-02-06 16:19:44 +0300

    update PlayerStatsWindowView

--- src/Asteroids/Assets/_Project/Code/UI/PlayerStatsWindow/PlayerStatsWindowView.cs
+++ src/Asteroids/Assets/_Project/Code/UI/PlayerStatsWindow/PlayerStatsWindowView.cs
@@ -1,4 +1,6 @@
-﻿using TMPro;
+﻿using Cysharp.Threading.Tasks;
+using LitMotion;
+using TMPro;
 using UnityEngine;
 
 namespace _Project.Code.UI.PlayerStatsWindow
@@ -10,7 +12,26 @@ namespace _Project.Code.UI.PlayerStatsWindow
         [SerializeField] private TextMeshProUGUI _velocity;
         [SerializeField] private TextMeshProUGUI _laserCharges;
         [SerializeField] private TextMeshProUGUI _laserCooldown;
+
+        [Header("AnimationSettings")]
+        [SerializeField] private float _durationMoveAnim;
+
+        private RectTransform _rect;
+
+        private Vector2 _originPosition;
+        private Vector2 _targetPosition;
         
+        private void Awake()
+        {
+            _rect = GetComponent<RectTransform>();
+            
+            _originPosition = _rect.anchoredPosition; 
+            _targetPosition = new Vector2(-_rect.anchoredPosition.x, _rect.anchoredPosition.y);
+        }
+
+        private void Start() => 
+            PlayEntranceAnimation();
+
         public void SetPosition(Vector3 value) => 
             _position.text = $"Position - " + $"X: {value.x:0.00} " + $"Y: {value.y:0.00}";
 
@@ -26,6 +47,29 @@ namespace _Project.Code.UI.PlayerStatsWindow
         public void SetLaserCooldown(float value) =>
             _laserCooldown.text = $"Laser Cooldown - {value:0}";
 
-        public void Destroy() => Destroy(gameObject);
+        public async UniTaskVoid Destroy()
+        {
+            await PlayHideAnimation();
+            Destroy(gameObject);
+        }
+
+        private void PlayEntranceAnimation()
+        {
+            LMotion.Create(_originPosition, _targetPosition, _durationMoveAnim)
+                .WithDelay(0.2f)
+                .WithEase(Ease.OutBack)
+                .Bind(pos => _rect.anchoredPosition = pos)
+                .AddTo(this);
+        }
+
+        private UniTask PlayHideAnimation()
+        {
+            return LMotion.Create(_targetPosition, _originPosition, _durationMoveAnim)
+                .WithDelay(0.2f)
+                .WithEase(Ease.OutBack)
+                .Bind(pos => _rect.anchoredPosition = pos)
+                .AddTo(this)
+                .ToUniTask();
+        }
     }
 }
