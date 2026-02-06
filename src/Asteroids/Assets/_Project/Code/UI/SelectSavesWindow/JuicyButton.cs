d00b296366430fa527e92bbc3b01331f48d373fa
ReLaine <pupkin_vasya1998@mail.ru>
2026-02-06 22:03:00 +0300

    replace buttons on JuicyButton

--- src/Asteroids/Assets/_Project/Code/UI/SelectSavesWindow/JuicyButton.cs
+++ src/Asteroids/Assets/_Project/Code/UI/SelectSavesWindow/JuicyButton.cs
@@ -1,9 +1,13 @@
 ﻿using LitMotion;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class JuicyButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    [SerializeField] private Sprite _onMouseEnterSprite;
+    [SerializeField] private Sprite _onMouseExitSprite;
+
     [Header("AnimationSettings")]
     [SerializeField] private float _bounceScale;
     [SerializeField] private float _durationBounce;
@@ -11,15 +15,26 @@ public class JuicyButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
     private MotionHandle _handle;
     
     private RectTransform _rect;
+    
+    private Image _image;
 
-    private void Awake() => 
+    private void Awake()
+    {
         _rect = GetComponent<RectTransform>();
+        _image = GetComponent<Image>();
+    }
 
-    public void OnPointerEnter(PointerEventData eventData) => 
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _image.sprite = _onMouseEnterSprite;
+        
         _handle = AnimatePulse();
+    }
 
     public void OnPointerExit(PointerEventData eventData)
     {
+        _image.sprite = _onMouseExitSprite;
+        
         _handle.Cancel();
         
         LMotion.Create(_rect.localScale, Vector3.one, _durationBounce)
